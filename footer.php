78ffaca1a6787db74628f15ddf490f8c9cd80b79
unknown <santoserick@gmail.com>
2020-08-27 23:04:11 -0300

    smoth scroll adicionado para teste na pagina de testes

--- footer.php
+++ footer.php
@@ -51,6 +51,10 @@
     <!-- ADD FOOTER WORDPRESS -->
     <?php wp_footer(); ?>
 
+    <script
+  src="https://code.jquery.com/jquery-3.5.1.js"
+  integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc="
+  crossorigin="anonymous"></script>
     <script>
       $(function(){
         $('.testimonial__list').slick({
