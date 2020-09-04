20ce1260aa5fdae24b103276425a0bfe8908b48d
Matheus Henrique <matheushds.br@gmail.com>
2020-09-04 16:16:06 -0300

    texto do h3 alterado para -> pesquisar postagens

--- sidebar.php
+++ sidebar.php
@@ -1,7 +1,7 @@
 <div class="col-lg-3 col-md-4">
     <div class="sidebar">
         <header>
-            <h3><i class="fas fa-sort"></i> Filtre por categoria:</h3>
+            <h3><i class="fas fa-sort"></i> Pesquisar postagens:</h3>
 
             <div class="search mt-3">
                 <?php get_search_form(); ?>
