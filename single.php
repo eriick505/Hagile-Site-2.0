89769462fc50d1ce24b9f831681449c48ee7ca5e
unknown <santoserick@gmail.com>
2020-09-05 17:46:17 -0300

    pequenos ajustes feito na pagina de single

--- single.php
+++ single.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 
-  <div class="expand-container my-5">
+  <div class="expand-container blog">
     <div class="row">
 
       <main class="col-lg-9 col-md-8 mb-3">
@@ -41,9 +41,45 @@
           <a href="#" class="btn btn-primary btn__had"><i class="fas fa-chevron-left"></i> Voltar para as notícias</a>
       </div>
 
-      <div class="col-12 mt-5">
+      <div class="col-12 my-5">
         <?php get_template_part( 'comments' ); ?>
       </div>
+
+      <div class="col-12 mb-3">
+        <h3>Você deve querer ver também...</h3>
+      </div>
+        
+      <?php 
+          $my_args_depo = array(
+              'post_type' => 'post',
+              'posts_per_page' => 3,
+          );
+
+          $my_query_depo = new WP_Query($my_args_depo);
+          
+          if($my_query_depo->have_posts()) : 
+          while($my_query_depo->have_posts()) : $my_query_depo->the_post();
+      ?>
+
+          <div class="col-lg-4 col-md-6">
+              <article class="blog__card" style="background-image: url('<?php the_post_thumbnail_url( $size ); ?>');">
+                  <div class="overlay"></div>
+                  
+                  <div class="blog__body">
+                      <span><i class="far fa-calendar-alt"></i> <?php echo get_the_date('d/m/y'); ?> </span>
+                      <a href="<?php the_permalink(); ?>">
+                          <h4><?php the_title(); ?></h4>
+                      </a>
+                      <a class="more" href="<?php the_permalink(); ?>">Saiba mais <i class="fas fa-chevron-right"></i></a>
+                  </div>
+              </article>
+          </div>
+
+      <?php endwhile; ?>
+
+      <?php else : get_404_template(); endif; ?>
+      <?php wp_reset_query(); ?>
+
 
     </div>
   </div>
